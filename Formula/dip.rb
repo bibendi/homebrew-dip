d669a69127c2b5f79d1fd0de91370c6e16db3650
Michail Merkushin <bibendi@bk.ru>
2016-11-17 21:16:38 +0500

    Release version 0.8.1

--- Formula/dip.rb
+++ Formula/dip.rb
@@ -2,8 +2,8 @@ class Dip < Formula
   desc "Docker Interaction Process"
   homepage "https://github.com/bibendi/dip"
   url "https://github.com/bibendi/dip.git",
-      tag: "0.8.0",
-      revision: "21d66c0dba4ff8b0230f01c537367ee7b89f2b61"
+      tag: "0.8.1",
+      revision: "9ff5dfa1403ec84ecc4983cc52401780cdad3a16"
 
   depends_on "crystal-lang" => :build
   depends_on "libyaml"
