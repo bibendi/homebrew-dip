7789a5f0a289cff1448ffedf2c1b4b8e1df8ef11
Misha Merkushin <merkushin.m.s@gmail.com>
2019-10-22 18:03:14 +0300

    Release 4.0.1

--- Formula/dip.rb
+++ Formula/dip.rb
@@ -1,15 +1,15 @@
 class Dip < Formula
   desc "dip CLI gives the native-like interaction with Docker-Compose applications"
   homepage "https://github.com/bibendi/dip"
-  version "4.0.0"
+  version "4.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bibendi/dip/releases/download/v4.0.0/dip-Darwin-x86_64"
-    sha256 "b494fe846dbd41862ca9a72728b1d6be879d6b1cc3bf6866448bdeb184c113c1"
+    url "https://github.com/bibendi/dip/releases/download/v4.0.1/dip-Darwin-x86_64"
+    sha256 "de62898de3a7758853f5171c2b72f75d26ae8122120d4e88c4ad96d739e34100"
   elsif OS.linux?
-    url "https://github.com/bibendi/dip/releases/download/v4.0.0/dip-Linux-x86_64"
-    sha256 "92d44b3832563926817f763e5d6c1c012c557b814ff0579cfd40bfe3a9bebed4"
+    url "https://github.com/bibendi/dip/releases/download/v4.0.1/dip-Linux-x86_64"
+    sha256 "be989489c97b5f7b59214fb4ebebdf4b79da797edbefc98607e3950fb7d0df30"
   end
 
   def install
