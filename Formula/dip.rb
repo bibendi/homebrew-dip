311888e28ba54086d77f8f6203ce50c1d014b35d
Misha Merkushin <merkushin.m.s@gmail.com>
2020-08-21 10:30:35 +0300

    Release 6.1.0

--- Formula/dip.rb
+++ Formula/dip.rb
@@ -1,5 +1,5 @@
 class Dip < Formula
-  VERSION = "6.0.0".freeze
+  VERSION = "6.1.0".freeze
 
   desc "dip CLI gives the native-like interaction with Docker-Compose applications"
   homepage "https://github.com/bibendi/dip"
@@ -8,10 +8,10 @@ class Dip < Formula
 
   if OS.mac?
     url "https://github.com/bibendi/dip/releases/download/v#{VERSION}/dip-Darwin-x86_64"
-    sha256 "d22b31869f49f8ccd04e38fec534d9970bcbab980e00853b400e47300638af8e"
+    sha256 "d0ab691c000f2ca2e1d78ac6d43f14919b14d5944fb25f397138348d045fd081"
   elsif OS.linux?
     url "https://github.com/bibendi/dip/releases/download/v#{VERSION}/dip-Linux-x86_64"
-    sha256 "0e625c21ea6e0b54c8d79cae09aeda35f1a8979a32190d039e15d2bcf71cf45e"
+    sha256 "f944f9f0a79b7ddfb272b34bd5812f2ae9b3d3611e36b41b2cb1c7e5359863ff"
   end
 
   def install
